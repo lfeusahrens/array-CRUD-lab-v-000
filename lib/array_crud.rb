b58e5224a55d64fff49cb6f54700ffaddb52e19c
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-09-05 20:43:26 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, 1)
+  add_element_to_end_of_array(array, 1).push(1)
 end
 
 def add_element_to_start_of_array(array, element)
